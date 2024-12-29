94415516a5f78c19c7fec1740f1387f0f2e91c6f
rajveer mehta <rajveermehta1021@gmail.com>
2024-12-29 15:19:08 +0530

    Added add and time_task tasks as examples of parallel execution.

--- jobs/tasks.py
+++ jobs/tasks.py
@@ -3,18 +3,31 @@ import time
 import logging
 
 logging.basicConfig(
-    level=logging.INFO,               
-    format="%(asctime)s - %(levelname)s - %(message)s",  
-    datefmt="%Y-%m-%d %H:%M:%S"      
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
 )
 
 @shared_task
 def add(x, y):
     try:
         result = x + y
-        time.sleep(10)
+        time.sleep(30)
         logging.info(f"Task completed successfully. Result: {result}")
         return result
     except Exception as e:
         logging.error(f"Task failed with error: {e}")
+        raise
+
+@shared_task(
+    max_retries=3,  # Maximum number of retries
+    default_retry_delay=5,  # Wait 5 seconds before retrying
+    autoretry_for=(Exception,),  # Exceptions to trigger a retry
+)
+def time_task(time_period):
+    try:
+        time.sleep(100)
+        logging.info(f"This is a time-consuming task but will start parallelly: {time_period}")
+    except Exception as e:
+        logging.error(f"There is an error: {e}")
         raise
--- jobs/views.py
+++ jobs/views.py
@@ -1,8 +1,12 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .tasks import add
+from .tasks import add, time_task
 
 class AddView(APIView):
     def get(self, request, *args, **kwargs):
-        result = add.delay(4, 6)  
-        return Response({"task_id": result.id})
+        result = add.delay(4, 6)
+        result1 = time_task.delay(10)
+        return Response({
+            "add_task_id": result.id,
+            "time_task_id": result1.id
+        })
