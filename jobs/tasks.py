35e99ecf5271ca8067df0bb9be9f3b05093eec80
rajveer mehta <rajveermehta1021@gmail.com>
2024-12-30 00:47:25 +0530

    #Fix Added add and time_task tasks as examples of parallel execution.

--- jobs/tasks.py
+++ jobs/tasks.py
@@ -11,9 +11,10 @@ logging.basicConfig(
 @shared_task
 def add(x, y):
     try:
+        logging.info(f"Task1 started")
         result = x + y
         time.sleep(30)
-        logging.info(f"Task completed successfully. Result: {result}")
+        logging.info(f"Task1 completed successfully. Result: {result}")
         return result
     except Exception as e:
         logging.error(f"Task failed with error: {e}")
@@ -24,10 +25,11 @@ def add(x, y):
     default_retry_delay=5,  # Wait 5 seconds before retrying
     autoretry_for=(Exception,),  # Exceptions to trigger a retry
 )
-def time_task(time_period):
+def time_task():
     try:
+        logging.info(f"Task2 started")
         time.sleep(100)
-        logging.info(f"This is a time-consuming task but will start parallelly: {time_period}")
+        logging.info(f"Task2 completed successfully.")
     except Exception as e:
         logging.error(f"There is an error: {e}")
         raise
--- jobs/views.py
+++ jobs/views.py
@@ -4,8 +4,8 @@ from .tasks import add, time_task
 
 class AddView(APIView):
     def get(self, request, *args, **kwargs):
-        result = add.delay(4, 6)
-        result1 = time_task.delay(10)
+        result = add.apply_async([4, 6])
+        result1 = time_task.apply_async()
         return Response({
             "add_task_id": result.id,
             "time_task_id": result1.id
